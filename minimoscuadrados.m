f716972e49eb0d110ffe9a2b8e1ee4b1dbb3354b
Jesus Rodolfo Gil farias <jrgil.20@est.ucab.edu.ve>
2025-12-17 15:42:03 -0400

    add: minimos cuadrados

---
+++ minimoscuadrados.m
@@ -0,0 +1,36 @@
+% --- DATOS (Caso Diapositiva 16) ---
+x = [1; 2; 3; 4; 5];
+y = [2; 3; 5; 4; 6];
+n = 2;
+
+% --- PROCESAMIENTO ---
+x = x(:);
+y = y(:);
+
+% 1. Construcción de A
+A = [];
+for p = 0:n
+    A = [A, x.^p];
+end
+
+% IMPRIMIR MATRIZ A (Vandermonde)
+disp('--- Paso 1: Matriz de Diseño A (Vandermonde) ---');
+disp(A);
+
+% 2. Construcción de Normales
+M = A' * A;
+B = A' * y;
+
+% IMPRIMIR SISTEMA NORMAL
+disp('--- Paso 2: Matriz A transpuesta * A ---');
+disp(M);
+
+disp('--- Paso 3: Vector A transpuesta * y ---');
+disp(B);
+
+% 3. Solución
+coefs = A \ y;
+
+% IMPRIMIR RESULTADOS
+disp('--- Paso 4: Coeficientes Solución ---');
+disp(coefs);
